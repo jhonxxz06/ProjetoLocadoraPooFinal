2781f8095c164010bd70ea30494e0ce5ea42eabc
JOAO PEDRO SANTANA DE OLIVEIRA MENEZES <200038747@ucsal.br>
2025-11-21 10:15:55 -0300

    Projeto sem os extras

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+package org.ucsal.poo.pf20252.br;
+
+public class Cliente {
+    private String nome;
+    private String Cpf;
+    private int idade;
+
+    public Cliente(String nome, String cpf, int idade){
+        this.nome = nome;
+        this.Cpf = cpf;
+        this.idade = idade;
+    }
+}
---
+++ Locacao.java
@@ -0,0 +1,27 @@
+package org.ucsal.poo.pf20252.br;
+
+public class Locacao implements Locavel {
+   private String dataInicio;
+   private String dataFim;
+   private double valorTotal;
+   private Cliente cliente;
+   private Veiculo veiculo;
+
+   public Locacao(Cliente cliente, Veiculo veiculo, String inicio, String fim, int dias) {
+      this.cliente = cliente;
+      this.veiculo = veiculo;
+      dataInicio = inicio;
+      dataFim = fim;
+      this.valorTotal = veiculo.calcularValorLocacao(dias);
+   }
+
+   @Override
+   public void iniciarLocacao(){
+      System.out.println("Locação Iniciada!!");
+   }
+
+   @Override
+   public void finalizarLocacao(){
+      System.out.println("Locação Finalizada");
+   }
+}
